c3ba62163cc4fa0f09195868f960d20af47b968d
jemaynar <maynard.john.e@gmail.com>
2023-12-09 09:19:29 -0500

    Building out more tests and cleaning up test names.

--- src/UnitTests/Day1Tests.fs
+++ src/UnitTests/Day1Tests.fs
@@ -3,37 +3,49 @@
     open Xunit
     
     [<Fact>]
-    let ``Day1.getDigits when empty returns none`` () =
+    let ``getDigits when empty returns none`` () =
         let result = Day1.getDigits <| ""
         
         Assert.Equal<Option<IEnumerable<byte>>>(None, result)
         
     [<Fact>]
-    let ``Day1.getDigits when whitespace returns none`` () =
+    let ``getDigits when whitespace returns none`` () =
         let result = Day1.getDigits <| " "
         
         Assert.Equal<Option<IEnumerable<byte>>>(None, result)
         
     [<Fact>]
-    let ``Day1.getDigits when non-digit returns none`` () =
+    let ``getDigits when non-digit returns none`` () =
         let result = Day1.getDigits <| "a"
         
         Assert.Equal<Option<IEnumerable<byte>>>(None, result)
         
     [<Fact>]
-    let ``Day1.getDigits when digit returns some`` () =
+    let ``getDigits when digit returns some`` () =
         let result = Day1.getDigits <| "1"
         
         Assert.True(result.IsSome)
         
     [<Fact>]
-    let ``Day1.firstAndLastDigit when none return none`` () =
+    let ``firstAndLastDigit when none return none`` () =
         let result = Day1.firstAndLastDigit <| None
         
         Assert.True(result.IsNone)
         
     [<Fact>]
-    let ``Day1.firstAndLastDigit when some 1 returns (1, 1)`` () =
+    let ``firstAndLastDigit when some "[| 1 |]" returns (1, 1)`` () =
         let result = Day1.firstAndLastDigit <| Some([| 1uy |])
         
         Assert.Equal<byte * byte>(result.Value, (1uy, 1uy))
+        
+    [<Fact>]
+    let ``firstAndLastDigit when some "[| 1; 2 |]" returns (1 2)`` () =
+        let result = Day1.firstAndLastDigit <| Some([| 1uy; 2uy |])
+        
+        Assert.Equal<byte * byte>(result.Value, (1uy, 2uy))
+        
+    [<Fact>]
+    let ``firstAndLastDigit when some "[| 1; 3; 2 |]" returns (1 2)`` () =
+        let result = Day1.firstAndLastDigit <| Some([| 1uy; 3uy; 2uy |])
+        
+        Assert.Equal<byte * byte>(result.Value, (1uy, 2uy))
