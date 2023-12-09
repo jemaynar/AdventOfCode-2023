80bb2a325980207daf7332c63adebf6a0f6f72c0
jemaynar <maynard.john.e@gmail.com>
2023-12-09 07:45:12 -0500

    Some code / tests towards first step of day 1.

--- src/AOC-2023/Day1.fs
+++ src/AOC-2023/Day1.fs
@@ -1,4 +1,15 @@
 ﻿module Day1
+
+    open System
+
+    let getDigits (input: string) =
+        let result =
+            input |> Seq.filter(Char.IsDigit)
+            |> Seq.map(byte)
+        match result with
+        | s when Seq.isEmpty s -> None
+        | s -> Some(s)
+    
     let Execute =
         let lines = Common.getData ".\Data\input1.txt"
         
---
+++ src/UnitTests/Day1Tests.fs
@@ -0,0 +1,27 @@
+﻿module UnitTests.Day1Tests
+    open System.Collections.Generic
+    open Xunit
+    
+    [<Fact>]
+    let ``Day1.getDigits when empty returns none`` () =
+        let result = Day1.getDigits <| ""
+        
+        Assert.Equal<Option<IEnumerable<byte>>>(None, result)
+        
+    [<Fact>]
+    let ``Day1.getDigits when whitespace returns none`` () =
+        let result = Day1.getDigits <| " "
+        
+        Assert.Equal<Option<IEnumerable<byte>>>(None, result)
+        
+    [<Fact>]
+    let ``Day1.getDigits when non-digit returns none`` () =
+        let result = Day1.getDigits <| "a"
+        
+        Assert.Equal<Option<IEnumerable<byte>>>(None, result)
+        
+    [<Fact>]
+    let ``Day1.getDigits when digit returns some`` () =
+        let result = Day1.getDigits <| "1"
+        
+        Assert.True(result.IsSome)
