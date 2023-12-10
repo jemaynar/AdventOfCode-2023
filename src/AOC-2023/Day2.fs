c35ddfcbf942a2ff999ad3605f1db79fd852e4a6
jemaynar <maynard.john.e@gmail.com>
2023-12-10 15:06:09 -0500

    Adding types I think I'll need for Day2.fs.

--- src/AOC-2023/Day2.fs
+++ src/AOC-2023/Day2.fs
@@ -1,4 +1,17 @@
 ﻿module Day2
+
+    type Color =
+        | Red = 0
+        | Green = 1
+        | Blue = 3
+
+    type Draw = {
+        DrawColor: Color
+        Count : byte
+    }
+        
+    type Game = { GameNo: byte; Draws: Draw seq }
+
     let Execute (showData: bool) =
         let lines = Common.getData ".\Data\input2.txt"
 
